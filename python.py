04dcaca9dd6f47c463e795f86b6aeaa3ad925417
ShueLee8226 <35345741+ShueLee8226@users.noreply.github.com>
2018-05-09 08:37:39 -0500

    update

---
+++ python.py
@@ -0,0 +1,9 @@
+#hello
+#This is how you create python file
+#it's the same as shell script
+#instead of echo it's print
+#other then that every thing is the same just bit of change in some command
+
+print"hello world"
+test = "this is a variable"
+print test
